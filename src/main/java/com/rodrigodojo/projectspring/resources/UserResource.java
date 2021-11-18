23ef52a9deeb13d66bfa4d63676b341e7d367904
rodrigodojo <rodrigodoj@gmail.com>
2021-11-18 17:39:04 -0300

    Criando uma entidade e um Rest Controller simples

---
+++ src/main/java/com/rodrigodojo/projectspring/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.rodrigodojo.projectspring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigodojo.projectspring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Rodrigo", "rodrigo@exemplo.com", "99999999", "000000");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
